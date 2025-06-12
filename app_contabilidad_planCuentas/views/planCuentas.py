c5fafa30521f89af9ea2479510b42b24d7a3dfdf
Bryan <bxbr92@hotmail.com>
2025-06-12 12:39:17 -0500

    Quitado el replace de plan de cuentas, transaccion

--- app_contabilidad_planCuentas/views/planCuentas.py
+++ app_contabilidad_planCuentas/views/planCuentas.py
@@ -1043,10 +1043,8 @@ class crearTransaccionPlanBIOView(CreateView):
                             cuerpo.encabezadocuentaplan_id = encabezado.pk
                             cuerpo.cuenta_id = cuenta_id
                             cuerpo.detalle = i['detalle']
-                            debe_valor = i['debe'].replace(',', '.') if i.get('debe') else '0.00'
-                            cuerpo.debe = Decimal(debe_valor)
-                            haber_valor = i['haber'].replace(',', '.') if i.get('haber') else '0.00'
-                            cuerpo.haber = Decimal(haber_valor)
+                            cuerpo.debe = Decimal(i['debe']) if i.get('debe') else Decimal('0.00')
+                            cuerpo.haber = Decimal(i['haber']) if i.get('haber') else Decimal('0.00')
                             cuerpo.save()
                         except Exception as e:
                             # Si hay un error, revertir la transacción y devolver el error
