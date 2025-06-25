9a23e45b2c1877903ccb2ef5a36e2747e954fa58
Bryan <bxbr92@hotmail.com>
2025-06-25 12:38:47 -0500

    FALTA ARREGLAR USUARIO LOS PERMISOS Y TIPO MODULO

--- app_user/forms.py
+++ app_user/forms.py
@@ -1,158 +1,241 @@
 from django import forms
-from django.contrib.auth.models import Group, Permission
-from django.forms import *
-from app_user.models import User, Modulo, TipoModulo, GrupoModulo
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User, Group, Permission
+from django.forms import ModelForm
+from django.forms.widgets import (
+    CheckboxInput, ClearableFileInput, Select, SelectMultiple,
+    TextInput, EmailInput, PasswordInput, Textarea, NumberInput
+)
+from .models import Modulo, TipoModulo
 
 
 class UserForm(ModelForm):
-    groups = ModelMultipleChoiceField(
+    # Campos adicionales
+    image = forms.ImageField(required=False, label="Foto de Perfil")
+
+    # Campos para permisos
+    groups = forms.ModelMultipleChoiceField(
         queryset=Group.objects.all(),
         required=False,
-        widget=SelectMultiple(attrs={
-            'class': 'form-control',
-        }),
-        label='Grupos'
+        label="Grupos de Usuario",
+        widget=SelectMultiple(attrs={'class': 'form-control select2', 'multiple': 'multiple'})
     )
-    user_permissions = ModelMultipleChoiceField(
+
+    user_permissions = forms.ModelMultipleChoiceField(
         queryset=Permission.objects.all(),
         required=False,
-        widget=SelectMultiple(attrs={
-            'class': 'form-control',
-        }),
-        label='Permisos'
+        label="Permisos Específicos",
+        widget=SelectMultiple(attrs={'class': 'form-control', 'multiple': 'multiple'})
     )
 
     class Meta:
         model = User
-        fields = (
-            'first_name', 'last_name', 'email', 'username', 'password',
-            'is_superuser', 'is_active', 'is_staff', 'imagen',
-            'groups', 'user_permissions'
-        )
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_active', 'is_staff', 'is_superuser']
+
         widgets = {
-            'password': PasswordInput(render_value=True, attrs={
-                'class': 'form-control',
-                'placeholder': 'Ingrese una Contraseña',
-                'autocomplete': 'off'
-            }),
             'username': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Ingrese el nombre de usuario',
-                'autocomplete': 'off'
+                'placeholder': 'Ingresa el nombre de usuario'
             }),
             'first_name': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Ingrese el nombre',
-                'autocomplete': 'off'
+                'placeholder': 'Ingresa los nombres'
             }),
             'last_name': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Ingrese el apellido',
-                'autocomplete': 'off'
+                'placeholder': 'Ingresa los apellidos'
             }),
             'email': EmailInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Ingrese un correo electrónico',
-                'autocomplete': 'off'
+                'placeholder': 'Ingresa el correo electrónico'
             }),
             'is_superuser': CheckboxInput(attrs={
-                'class': 'form-check-input',
-                'style': 'transform: scale(0.55); margin-top: 3px;'
+                'class': 'custom-control-input'
             }),
             'is_active': CheckboxInput(attrs={
-                'class': 'form-check-input',
-                'style': 'transform: scale(0.55); margin-top: 3px;'
+                'class': 'custom-control-input'
             }),
             'is_staff': CheckboxInput(attrs={
-                'class': 'form-check-input',
-            }),
-            'imagen': ClearableFileInput(attrs={
-                'class': 'form-control'
+                'class': 'custom-control-input'
             }),
         }
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Personalizar widget para imagen
+        self.fields['image'].widget = ClearableFileInput(attrs={
+            'class': 'form-control-file'
+        })
 
-class GroupForm(ModelForm):
-    modulos = ModelMultipleChoiceField(
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label="Nombres")
+    last_name = forms.CharField(max_length=30, required=True, label="Apellidos")
+    email = forms.EmailField(required=True, label="Correo Electrónico")
+    image = forms.ImageField(required=False, label="Foto de Perfil")
+
+    groups = forms.ModelMultipleChoiceField(
+        queryset=Group.objects.all(),
+        required=False,
+        label="Grupos de Usuario",
+        widget=SelectMultiple(attrs={'class': 'form-control select2', 'multiple': 'multiple'})
+    )
+
+    user_permissions = forms.ModelMultipleChoiceField(
+        queryset=Permission.objects.all(),
+        required=False,
+        label="Permisos Específicos",
+        widget=SelectMultiple(attrs={'class': 'form-control', 'multiple': 'multiple'})
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',
+                  'is_active', 'is_staff', 'is_superuser')
+
+        widgets = {
+            'username': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingresa el nombre de usuario'
+            }),
+            'first_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingresa los nombres'
+            }),
+            'last_name': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingresa los apellidos'
+            }),
+            'email': EmailInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingresa el correo electrónico'
+            }),
+            'password1': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Ingresa la contraseña'
+            }),
+            'password2': PasswordInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Confirma la contraseña'
+            }),
+            'is_superuser': CheckboxInput(attrs={
+                'class': 'custom-control-input'
+            }),
+            'is_active': CheckboxInput(attrs={
+                'class': 'custom-control-input'
+            }),
+            'is_staff': CheckboxInput(attrs={
+                'class': 'custom-control-input'
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Personalizar widget para imagen
+        self.fields['image'].widget = ClearableFileInput(attrs={
+            'class': 'form-control-file'
+        })
+
+        # Asegurar que los permisos se carguen correctamente
+        self.fields['user_permissions'].queryset = Permission.objects.all().select_related('content_type')
+
+
+class GroupForm(forms.ModelForm):
+    modulos = forms.ModelMultipleChoiceField(
         queryset=Modulo.objects.filter(activo=True),
         required=False,
-        widget=CheckboxSelectMultiple(attrs={
-            'class': 'form-check-input',
-        }),
-        label='Módulos'
+        label="Módulos del Sistema",
+        widget=SelectMultiple(attrs={'class': 'form-control select2', 'multiple': 'multiple'})
     )
 
     class Meta:
         model = Group
         fields = ['name', 'permissions']
+        labels = {
+            'name': 'Nombre del Grupo',
+            'permissions': 'Permisos del Sistema'
+        }
         widgets = {
             'name': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Ingrese el nombre del grupo',
+                'placeholder': 'Ingresa el nombre del grupo'
             }),
             'permissions': SelectMultiple(attrs={
-                'class': 'form-control',
-            }),
-        }
-        labels = {
-            'name': 'Nombre del Grupo',
-            'permissions': 'Permisos',
+                'class': 'form-control select2',
+                'multiple': 'multiple'
+            })
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        if self.instance.pk:
-            self.fields['modulos'].initial = Modulo.objects.filter(
-                grupomodulo__grupo=self.instance
-            )
 
-
-class ModuloForm(ModelForm):
+class ModuloForm(forms.ModelForm):
     class Meta:
         model = Modulo
-        fields = ['nombre', 'descripcion', 'url', 'icono', 'tipo', 'activo', 'orden']
+        fields = ['nombre', 'url', 'icono', 'descripcion', 'tipo', 'orden', 'activo']
+        labels = {
+            'nombre': 'Nombre del Módulo',
+            'url': 'URL del Módulo',
+            'icono': 'Icono',
+            'descripcion': 'Descripción',
+            'tipo': 'Tipo de Módulo',
+            'orden': 'Orden',
+            'activo': 'Activo'
+        }
         widgets = {
             'nombre': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Ingrese el nombre del módulo',
-            }),
-            'descripcion': Textarea(attrs={
-                'class': 'form-control',
-                'rows': 3,
-                'placeholder': 'Descripción del módulo',
+                'placeholder': 'Nombre del módulo'
             }),
             'url': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'URL del módulo',
+                'placeholder': '/ruta/del/modulo/'
             }),
             'icono': TextInput(attrs={
                 'class': 'form-control',
-                'placeholder': 'Clase del icono (ej: fas fa-home)',
-            }),
-            'tipo': Select(attrs={
-                'class': 'form-control',
-            }),
-            'activo': CheckboxInput(attrs={
-                'class': 'form-check-input',
-            }),
-            'orden': NumberInput(attrs={
-                'class': 'form-control',
-            }),
-        }
-
-
-class TipoModuloForm(ModelForm):
-    class Meta:
-        model = TipoModulo
-        fields = ['nombre', 'descripcion']
-        widgets = {
-            'nombre': TextInput(attrs={
-                'class': 'form-control',
-                'placeholder': 'Ingrese el nombre del tipo',
+                'placeholder': 'fas fa-icon'
             }),
             'descripcion': Textarea(attrs={
                 'class': 'form-control',
                 'rows': 3,
-                'placeholder': 'Descripción del tipo de módulo',
+                'placeholder': 'Descripción del módulo'
             }),
+            'tipo': Select(attrs={
+                'class': 'form-control'
+            }),
+            'orden': NumberInput(attrs={
+                'class': 'form-control',
+                'min': '1'
+            }),
+            'activo': CheckboxInput(attrs={
+                'class': 'custom-control-input'
+            })
+        }
+
+
+class TipoModuloForm(forms.ModelForm):
+    class Meta:
+        model = TipoModulo
+        fields = ['nombre', 'descripcion', 'orden', 'activo']
+        labels = {
+            'nombre': 'Nombre del Tipo',
+            'descripcion': 'Descripción',
+            'orden': 'Orden',
+            'activo': 'Activo'
+        }
+        widgets = {
+            'nombre': TextInput(attrs={
+                'class': 'form-control',
+                'placeholder': 'Nombre del tipo de módulo'
+            }),
+            'descripcion': Textarea(attrs={
+                'class': 'form-control',
+                'rows': 3,
+                'placeholder': 'Descripción del tipo de módulo'
+            }),
+            'orden': NumberInput(attrs={
+                'class': 'form-control',
+                'min': '1'
+            }),
+            'activo': CheckboxInput(attrs={
+                'class': 'custom-control-input'
+            })
         }
